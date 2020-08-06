54c26951474ad512e3bea42356925e5d37d0b97d
fionabeatricewong <fionabeatricewong@gmail.com>
2020-08-06 09:20:03 -0400

    Added event listeners for auth

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,13 @@
 'use strict'
 
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
+// Require:
+const authEvents = require('./auth/events')
 
-// use require without a reference to ensure a file is bundled
-// require('./example')
-
+// Event Listeners:
 $(() => {
-  // your JS code goes here
+  // Authentication
+  $('#sign-up').on('submit', authEvents.onSignUp)
+  $('#sign-in').on('submit', authEvents.onSignIn)
+  $('#sign-out').on('submit', authEvents.onSignOut)
+  $('#change-password').on('submit', authEvents.onChangePassword)
 })
