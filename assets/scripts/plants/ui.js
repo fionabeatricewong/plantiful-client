de9b85365e07765b784c224b13c25be60b55614f
fionabeatricewong <fionabeatricewong@gmail.com>
2020-08-07 18:25:05 -0400

    Updated to fix plant index from printing each time button is clicked

--- assets/scripts/plants/ui.js
+++ assets/scripts/plants/ui.js
@@ -4,11 +4,31 @@
 const showPlantsTemplate = require('../templates/plant-collection.handlebars')
 // const store = require('../store')
 
+const indexPlantsSuccess = (data) => {
+  const showPlantsHtml = showPlantsTemplate({ plants: data.plants })
+  // if no plants in collection:
+  // if (plants === '') {
+  //   $('#message').text('no plants in your collection yet!')
+  // } else {
+  $('#message').text('look at all your plant babies!')
+  // Hide "outdated" plant collection:
+  $('#content').empty()
+  $('#content').append(showPlantsHtml)
+  $('#content').show()
+}
+// }
+
+const indexPlantsFailure = () => {
+  $('#message').text('couldn\'t show all plants')
+}
+
 const createPlantSuccess = (data) => {
   const showPlantsHtml = showPlantsTemplate({ plants: data.plants })
-  $('.content').append(showPlantsHtml)
 
   $('#message').text('new plant added to your collection!')
+  $('#content').append(showPlantsHtml)
+  // "refresh" and show content/index:
+  //
 
   $('form').trigger('reset')
 }
@@ -17,20 +37,10 @@ const createPlantFailure = () => {
   $('#message').text('couldn\'t add new plant!')
 }
 
-const indexPlantsSuccess = (data) => {
-  const showPlantsHtml = showPlantsTemplate({ plants: data.plants })
-  $('.content').append(showPlantsHtml)
-  $('#message').text('look at all your plant babies!')
-}
-
-const indexPlantsFailure = () => {
-  $('#message').text('couldn\'t show all plants')
-}
-
 const destroyPlantSuccess = () => {
   $('#message').text('bye plant!')
-  $('#content').hide()
-  $('#content').show()
+  // $('#content').hide()
+  // $('#content').show()
 }
 
 const destroyPlantFailure = () => {
