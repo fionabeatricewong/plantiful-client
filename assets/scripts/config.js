ad18c5c9b13d1bd46169e66b8b69c027e2777501
fionabeatricewong <fionabeatricewong@gmail.com>
2020-08-10 10:58:21 -0400

    Removed '/' at the end of heroku url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://morning-lake-31674.herokuapp.com/',
+  production: 'https://morning-lake-31674.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
